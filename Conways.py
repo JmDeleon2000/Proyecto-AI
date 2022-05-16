338953711b0e157e7fb07b9c668b0a9ea3657de4
JmDeleon2000 <54918136+JmDeleon2000@users.noreply.github.com>
2022-05-16 00:08:04 -0600

    optimized padding resetting

--- Conways.py
+++ Conways.py
@@ -1,4 +1,3 @@
-import json
 import time
 import numpy as np
 import pygame
@@ -18,22 +17,15 @@ class conways:
         self.x_bound = self.x_size - 1
         self.y_bound = self.y_size - 1
 
-
         self.square_off = int(screen_size[0]/(self.x_size-2))
 
-        self.buff_1 = np.random.choice(a = [False, True, True], size = (self.x_size, self.y_size))
-        for y, e in enumerate(self.buff_1):
-            for x, val in enumerate(e):
-                if (y == 0 or x == 0 or x == self.x_bound or y == self.y_bound):
-                    self.buff_1[y, x] = False
+        self.buff_1 = np.random.choice(a = [False, False, False, False, False, True, True], 
+        size = (self.x_size, self.y_size))
         self.buff_2 = np.copy(self.buff_1)
-
-        
 
         self.buff = []
         self.buff.append(self.buff_1)
         self.buff.append(self.buff_2)
-
 
         self.reset(self.buff_1)
 
@@ -47,10 +39,14 @@ class conways:
             for x in range(1, self.x_bound):
                 if self.buff_1[y, x]:
                     self.moves += 1
-        for y, e in enumerate(self.buff_1):
-            for x, val in enumerate(e):
-                if (y == 0 or x == 0 or x == self.x_bound or y == self.y_bound):
-                    self.buff_1[y, x] = False
+        #reset padding
+        for x in range(self.x_size):
+            self.buff_1[0, x] = False
+            self.buff_1[x, 0] = False
+            self.buff_1[self.y_bound, x] = False
+            self.buff_1[x, self.y_bound] = False
+
+
         self.buff_2 = np.copy(self.buff_1)
 
 
@@ -85,16 +81,20 @@ class conways:
                     count == 3):
                         self.buff[1][y, x] = True
                         changed = True
-                        self.screen.fill(self.liveCellColor, 
-                        rect=(self.square_off*(y-1), self.square_off*(x-1), self.square_off, self.square_off))
-                    if (self.buff[0][y, x] and 
-                    count < 2 or count > 3):
+                        
+                    if (self.buff[0][y, x] and count < 2 or count > 3):
                         self.buff[1][y, x] = False
                         changed = True
-                        #self.screen.fill(self.deadCellColor, 
-                        #rect=(self.square_off*(y-1), self.square_off*(x-1), self.square_off, self.square_off))
+                    if self.buff[0][y, x]:
+                        self.screen.fill(self.liveCellColor, 
+                        rect=(self.square_off*(y-1), self.square_off*(x-1), 
+                        self.square_off, self.square_off))
 
+
+
+            
             pygame.display.flip()
+            #time.sleep(1)
             if (self.buff[0] is self.buff_1):
                 self.buff[0] = self.buff_2
                 self.buff[1] = self.buff_1
@@ -109,7 +109,6 @@ class conways:
                 for y in range(1, self.y_bound):
                     for x in range(1, self.x_bound):
                         if self.buff[0][y, x]:
-                            print(x)
                             alive = True
                             break
                     if alive:
@@ -117,24 +116,21 @@ class conways:
                 if alive:
                     return float('inf')
                 else:
-                    print(i)
                     return i**2 - self.moves
         return self.max_it**2 - self.moves
 
 
 
 
-test = conways(size = 512, max_it=1000)
+test = conways(size = 510, max_it=1000)
 print(test.run())
 
-x = np.zeros(shape=(514, 514), dtype=bool)
-x[10, 10] = True
-x[10, 11] = True
-x[11, 10] = True
-x[11, 11] = True
+x = np.zeros(shape=(512, 512), dtype=bool)
+x[100, 100] = True
+x[100, 101] = True
+x[101, 100] = True
+x[101, 101] = True
 
-#test.reset(np.random.choice(a = [False for i in range(10)]+[True], size = (514, 514)))
-#print(test.run())
 
 test.reset(x)
 print(test.run())
